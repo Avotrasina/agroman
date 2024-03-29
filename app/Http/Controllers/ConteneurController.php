cb039f1f8fba1da3f8792f49cbccf805712a12d2
Avotrasina <favotrasina@gmail.com>
2024-03-29 17:53:53 +0300

    Generating Models, Factory, Resources

---
+++ app/Http/Controllers/ConteneurController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Conteneur;
+use Illuminate\Http\Request;
+
+class ConteneurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Conteneur $conteneur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Conteneur $conteneur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Conteneur $conteneur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Conteneur $conteneur)
+    {
+        //
+    }
+}
