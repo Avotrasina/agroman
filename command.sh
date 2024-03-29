1568e853ca422d592610a2de43d37a3cd4a0b506
RandrianantenainaBearisoa <rabearisoabex@gmail.com>
2024-03-29 17:45:38 +0300

    command

--- command.sh
+++ command.sh
@@ -1,6 +1,5 @@
 # Définir les noms des modèles dans un tableau
 MODELS=("Produit" "Categorie" "Destination" "Provenance" "Stocker" "Conteneur" "TypeConteneur" "Historique")
-
 # Boucle à travers les noms des modèles et exécuter la commande Artisan pour chaque modèle
 for MODEL in "${MODELS[@]}"
 do
