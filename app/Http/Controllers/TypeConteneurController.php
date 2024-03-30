3d7c6cb5c99148b09706c675ab507a27f2d1ca6d
RandrianantenainaBearisoa <rabearisoabex@gmail.com>
2024-03-30 11:26:33 +0300

    migration + factory

---
+++ app/Http/Controllers/TypeConteneurController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeConteneur;
+use Illuminate\Http\Request;
+
+class TypeConteneurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TypeConteneur $typeConteneur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TypeConteneur $typeConteneur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TypeConteneur $typeConteneur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TypeConteneur $typeConteneur)
+    {
+        //
+    }
+}
